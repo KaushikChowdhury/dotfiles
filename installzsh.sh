eaf9ba3f43e252a77bf4173b0f284bb09a12f21c
Kaushik Chowdhury <chowdhury.kaushik@gmail.com>
2022-05-02 09:16:47 +0530

    Update installzsh.sh

--- installzsh.sh
+++ installzsh.sh
@@ -4,7 +4,7 @@ sudo apt install zsh -y
 
 sudo apt-get install build-essential curl file git -y
 
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -14,9 +14,9 @@ sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
 
 source ~/.zshrc
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 
 echo "plugins=(dirhistory history colored-man-pages jsontools python pip git zsh-autosuggestions zsh-syntax-highlighting)">> ~/.zshrc
